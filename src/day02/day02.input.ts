b0291c966301ec28f199de9d82dbefd1c9b9e721
Paulius Kigas <876738+pauliokas@users.noreply.github.com>
2022-12-03 16:14:00 +0200

    Fix parsing of day02 input

--- src/day02/day02.input.ts
+++ src/day02/day02.input.ts
@@ -1,6 +1,6 @@
 export type Pair = ['A' | 'B' | 'C', 'X' | 'Y' | 'Z'];
 
 export default (input: string): Pair[] => {
-  const lines = input.split('\n');
-  return lines.filter((line) => line !== '').map((line) => line.split(' ') as Pair);
+  const lines = input.trim().split('\n');
+  return lines.map((line) => line.split(' ') as Pair);
 };
